3ea404455f7f0c3dc5be5634210fd0efcb1ba14c
Jian Liu <onanying@126.com>
2019-07-08 19:23:18 +0800

    优化

--- src/View.php
+++ src/View.php
@@ -2,10 +2,6 @@
 
 namespace Mix\View;
 
-use Mix\Bean\BeanInjector;
-use Mix\Http\Message\Response;
-use Mix\Http\Message\Stream\ContentStream;
-
 /**
  * Class View
  * @package Mix\View
@@ -15,51 +11,43 @@ class View
 {
 
     /**
-     * @var Response
-     */
-    public $response;
-
-    /**
      * @var string
      */
-    public $layout = 'main';
+    public $layout = '';
 
     /**
      * View constructor.
      * @param array $config
      */
-    public function __construct(array $config)
+    public function __construct($layout = 'main')
     {
-        BeanInjector::inject($this, $config);
-        $this->response->withContentType('text/html', 'utf-8');
+        $this->layout = $layout;
     }
 
     /**
      * 渲染视图 (包含布局)
-     * @param $name
+     * @param string $name
      * @param array $data
-     * @return Response
+     * @return string
      */
-    public function render($name, $data = []): Response
+    public function render(string $name, array $data = []): string
     {
         $layout          = $this->layout;
         $renderer        = new Renderer();
         $data['content'] = $renderer->render($name, $data);
-        $html            = $renderer->render("layouts.{$layout}", $data);
-        return $this->response->withBody(new ContentStream($html));
+        return $renderer->render("layouts.{$layout}", $data);
     }
 
     /**
      * 渲染视图 (不包含布局)
-     * @param $name
+     * @param string $name
      * @param array $data
-     * @return Response
+     * @return string
      */
-    public function renderPartial($name, $data = []): Response
+    public function renderPartial(string $name, array $data = []): string
     {
         $renderer = new Renderer();
-        $html     = $renderer->render($name, $data);
-        return $this->response->withBody(new ContentStream($html));
+        return $renderer->render($name, $data);
     }
 
 }
